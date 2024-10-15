dade05df5275911d3a4326a3d89cdd8f1ab85fac
Naman Tamrakar <namantam1@gmail.com>
2024-10-16 05:18:17 +0530

    Update icons

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a6bd3f1de158bb61090e0c8053df93a10cb548
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3127626721"
+    serviceWorkerVersion: "4069121850"
   }
 });
